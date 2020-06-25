80cbe6b40adec2cf06a36c0df2e33053498d46d6
Serhii Yakymenko <m.serhiiyakymenko@gmail.com>
2020-06-25 16:16:41 -0400

    Feat: added in gulpfile.js gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,26 @@
-var gulp        = require('gulp');
+var {src, dest, watch}        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
 // Static server
-gulp.task('browser-sync', function() {
+  function bs() {
+    serveSass();
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./scss/**/*.scss", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
+
+// Compile sass into CSS & auto-inject into browsers
+ function serveSass() {
+  return src("./scss/*.scss")
+      .pipe(sass())
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
