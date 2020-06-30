155d08bb76cbf5b4bb27db793c055d1c7cb3297b
Serhii Yakymenko <m.serhiiyakymenko@gmail.com>
2020-06-29 20:10:42 -0400

    Hello

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,12 @@
-var {src, dest, watch}        = require('gulp');
+var gulp = require("gulp");
 var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
 
 // Static server
-  function bs() {
-    serveSass();
+gulp.task('browser-sync', function() {
   browserSync.init({
       server: {
-          baseDir: "./"
+          baseDir: "./",
       }
   });
-  watch("./*.html").on('change', browserSync.reload);
-  watch("./scss/**/*.scss", serveSass);
-  watch("./js/*.js").on('change', browserSync.reload);
-};
-
-// Compile sass into CSS & auto-inject into browsers
- function serveSass() {
-  return src("./scss/*.scss")
-      .pipe(sass())
-      .pipe(dest("./css"))
-      .pipe(browserSync.stream());
-};
-
-exports.serve = bs;
+  gulp.watch("./*.html").on('change', browserSync.reload);
+});
