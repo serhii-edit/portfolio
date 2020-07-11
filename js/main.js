53cfb24f9d1269f3f8354f32bba9c8bbce68e548
Serhii Yakymenko <m.serhiiyakymenko@gmail.com>
2020-07-11 16:27:26 -0400

    Feat: added validation for form & mask

--- js/main.js
+++ js/main.js
@@ -27,3 +27,62 @@ window.onclick = function (event) {
 // };
 
 // modal.addEventListener("onkeydown == 27", toggleModal);
+
+
+// xxxxxxxxxxx
+
+
+// validate & mask
+
+$(".contact-form").validate({
+  errorClass: "error-form",
+  rules: {
+    // simple rule, converted to {required:true}
+    userName: {
+      required: true,
+      minlength: 2,
+    },
+    userPhone: {
+      required: true,
+      minlength: 17,
+    },
+    userObject: {
+      required: true,
+      minlength: 3,
+    },
+    userMassage: {
+      required: true,
+      minlength: 150,
+      maxlength: 350,
+    },
+    // compound rule
+    userEmail: {
+      required: true,
+      email: true,
+    },
+  },
+// xxxxxxxxxxxxxxxxxxxxx
+    messages: {
+      userMassage: {
+       required: "Massage is required",
+       minlength: "Type more...",
+       maxlength: "Little Shorter",
+      },
+      userName: {
+        required: "Name is required",
+        minlength: "Not shorter than 1"
+      },
+      userPhone: "Phone is required",
+      userObject: {
+        required: "Object is required",
+        minlength: "Not shorter than 3"
+      },
+      userEmail: {
+        required: "Email is required",
+        email: "Type in format: name@domain.com",
+      },
+  },
+});
+
+// mask for phone
+$('[type=tel]').mask('+1 (000) 000-0000', {placeholder: "+1 (___) ___-____"});
